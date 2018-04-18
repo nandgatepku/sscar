eb61757464ca8865d028bcbea7e9b7b9575c958a
lindexin <dexinlin@vip.qq.com>
2018-04-18 19:40:37 +0800

    upload

--- application/common.php
+++ application/common.php
@@ -13,49 +13,10 @@
 
 use think\Controller;
 
-function http_send($url, $params, $method = 'GET', $header = array(), $multi = false){
-    $opts = array(
-        CURLOPT_TIMEOUT    => 30,
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_HTTPHEADER   => $header
-    );
-    /* 根据请求类型设置特定参数 */
-    switch(strtoupper($method)){
-        case 'GET':
-            $opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
-//            $user_info_url = sprintf("%s?appid=%s&secret=%s&js_code=%s&grant_type=%",$url,$params['appid'],$params['secret'],$params['js_code'],$params['$grant_type']);
-//            $weixin_user_data = json_decode(get_url($user_info_url));
-//            $session_key = $weixin_user_data->session_key;
-            break;
-        case 'POST':
-            //判断是否传输文件
-            $params = $multi ? $params : http_build_query($params);
-            $opts[CURLOPT_URL] = $url;
-            $opts[CURLOPT_POST] = 1;
-            $opts[CURLOPT_POSTFIELDS] = $params;
-            break;
-//        default:
-//            throw new Exception('不支持的请求方式！');
-    }
-//    print_r($opts);
-    /* 初始化并执行curl请求 */
-    $ch = curl_init();
-    curl_setopt_array($ch, $opts);
-    $data = curl_exec($ch);
-    $error = curl_error($ch);
-    curl_close($ch);
-//    if($error) throw new Exception('请求发生错误：' . $error);
-//    echo $data;
-    return $data;
-}
-
-
-
 function ret_message($message = "") {
     if ($message == "") return ['result'=>0, 'message'=>''];
     $ret = lang($message);
+print_r($ret);
 
     if (count($ret) != 2) {
         return ['result'=>-1,'message'=>'未知错误'];
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -13,179 +13,6 @@ class Index extends Base
         return $this->fetch('index');
     }
 
-    public function demo(){
-
-//        include_once "wxBizDataCrypt.php";
-        $appid = 'wx4f4bc4dec97d474b';
-        $sessionKey = 'tiihtNczf5v6AKRyjwEUhQ==';
-
-        $encryptedData="CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZM
-                QmRzooG2xrDcvSnxIMXFufNstNGTyaGS
-                9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+
-                3hVbJSRgv+4lGOETKUQz6OYStslQ142d
-                NCuabNPGBzlooOmB231qMM85d2/fV6Ch
-                evvXvQP8Hkue1poOFtnEtpyxVLW1zAo6
-                /1Xx1COxFvrc2d7UL/lmHInNlxuacJXw
-                u0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn
-                /Hz7saL8xz+W//FRAUid1OksQaQx4CMs
-                8LOddcQhULW4ucetDf96JcR3g0gfRK4P
-                C7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB
-                6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns
-                /8wR2SiRS7MNACwTyrGvt9ts8p12PKFd
-                lqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYV
-                oKlaRv85IfVunYzO0IKXsyl7JCUjCpoG
-                20f0a04COwfneQAGGwd5oa+T8yO5hzuy
-                Db/XcxxmK01EpqOyuxINew==";
-
-        $iv = 'r7BXXKkLb8qrSNn05n0qiA==';
-
-        $pc = new WXBizDataCrypt($appid, $sessionKey);
-        $errCode = $pc->decryptData($encryptedData, $iv, $data );
-
-        if ($errCode == 0) {
-            print($data . "\n");
-        } else {
-            print($errCode . "\n");
-        }
-    }
-
-
-    public  function user_login(){
-        $APPID = 'wx3dd12da36570cd80';
-        $AppSecret = '7799fedb17fd1463543704571be52cb4';
-        $wx_request_url = 'https://api.weixin.qq.com/sns/jscode2session';
-        $code = input("code");
-        $param = array(
-            'appid' => $APPID,
-            'secret' => $AppSecret,
-            'js_code' => $code,
-            'grant_type' => 'authorization_code'
-        );
-//        print_r($param);
-        // 一个使用curl实现的get方法请求
-        $arr = http_send($wx_request_url, $param, 'get');
-//        $user_info_url = sprintf("%s?appid=%s&secret=%s&js_code=%s&grant_type=%",$wx_request_url,$param['appid'],$param['secret'],$param['js_code'],$param['grant_type']);
-//        echo $user_info_url;
-//        $weixin_user_data = json_decode(get_url($user_info_url));
-//        $session_key = $weixin_user_data->session_key;
-        $arr = json_decode($arr,true);
-        if(isset($arr['errcode']) && !empty($arr['errcode'])){
-            return json(['code'=>'2','message'=>$arr['errmsg'],"result"=>null]);
-        }
-        $openid = $arr['openid'];
-        $session_key = $arr['session_key'];
-
-        // 数据签名校验
-        $signature = input("signature");
-        $signature2 = sha1($_GET['rawData'].$session_key);  //别用框架自带的input,会过滤掉必要的数据
-        if ($signature != $signature2) {
-            $msg = "shibai 1";
-            return json(['code'=>'2','message'=>'获取失败',"result"=>$msg]);
-        }
-
-        //开发者如需要获取敏感数据，需要对接口返回的加密数据( encryptedData )进行对称解密
-        $encryptedData = $_GET['encryptedData'];
-        $iv = $_GET['iv'];
-        include_once '../api/wxBizDataCrypt.php';
-        $pc = new \WXBizDataCrypt($APPID, $session_key);
-        $errCode = $pc->decryptData($encryptedData, $iv, $data);//其中$data包含用户的所有数据
-        if ($errCode != 0) {
-            return json(['code'=>'2','message'=>'获取失败',"result"=>null]);
-        }
-        /****** */
-        //写自己的逻辑： 操作数据库等操作
-        /****** */
-        //生成第三方3rd_session
-        $session3rd  = null;
-        $strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-        $max = strlen($strPol)-1;
-        for($i=0;$i<16;$i++){
-            $session3rd .=$strPol[rand(0,$max)];
-        }
-        return json(['code'=>'1','message'=>'获取成功',"result"=>$session3rd]);
-
-    }
-
-
-
-
-    public function sendCode(){
-        $APPID = 'wx3dd12da36570cd80';
-        $AppSecret = '7799fedb17fd1463543704571be52cb4';
-        $code = input('get.code');
-        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.$APPID.'&secret='.$AppSecret.'&js_code='.$code.'&grant_type=authorization_code';
-        $arr = $this -> vegt($url);
-
-        $arr = json_decode($arr,true);
-//        $openid = $arr['openid'];
-        $session_key = $arr['session_key'];
-
-        // 数字签名校验
-        $signature = input('get.signature');
-        $signature2 = sha1($_GET['rawData'].$session_key);
-        if($signature != $signature2){
-            echo "数字签名失败";
-            die;
-        }
-        // 获取信息，对接口进行解密
-        Vendor("PHP.wxBizDataCrypt");
-        $encryptedData = $_GET['encryptedData'];
-        $iv = $_GET['iv'];
-        if(empty($signature) || empty($encryptedData) || empty($iv)){
-            echo "传递信息不全";
-        }
-        include_once "../api/wxBizDataCrypt.php";
-
-        $pc = new \WXBizDataCrypt($APPID,$session_key);
-        $errCode = $pc->decryptData($encryptedData,$iv,$data);
-        if($errCode != 0){
-            echo "解密数据失败";
-            die;
-        }else {
-            $data = json_decode($data,true);
-            session('myinfo',$data);
-            $save['openid'] = $data['openId'];
-            $save['uname'] = $data['nickName'];
-            $save['unex'] = $data['gender'];
-            $save['address'] = $data['city'];
-            $save['time'] = time();
-            $map['openid'] = $data['openId'];
-            !empty($data['unionId']) && $save['unionId'] = $data['unionId'];
-
-//            $res = Db::name('user') -> where($map) -> find();
-//            if(!$res){
-//                $db = Db::name('user') -> insert($save);
-//                if($db !== false){
-//                    echo "保存用户成功";
-//                }else{
-//                    echo "error";
-//                }
-//            }else{
-//                echo "用户已经存在";
-//            }
-        }
-        //生成第三方3rd_session
-        $session3rd  = null;
-        $strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-        $max = strlen($strPol)-1;
-        for($i=0;$i<16;$i++){
-            $session3rd .=$strPol[rand(0,$max)];
-        }
-        return json(['code'=>'1','message'=>'获取成功',"result"=>$session3rd]);
-    }
-    public function vegt($url){
-        $info = curl_init();
-        curl_setopt($info,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($info,CURLOPT_HEADER,0);
-        curl_setopt($info,CURLOPT_NOBODY,0);
-        curl_setopt($info,CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($info,CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($info,CURLOPT_URL,$url);
-        $output= curl_exec($info);
-        curl_close($info);
-        return $output;
-    }
-
     public function wxLogin() {
         /**
          * 3.小程序调用server获取token接口, 传入code, rawData, signature, encryptData.
@@ -215,13 +42,13 @@ class Index extends Base
         $res = makeRequest($wx_request_url, $params);
 
         if ($res['code'] !== 200 || !isset($res['result']) || !isset($res['result'])) {
-//            return json(ret_message('requestTokenFailed'));
-            return $res['result'];
+            return json(ret_message('requestTokenFailed'));
+//            return $res['result'];
         }
         $reqData = json_decode($res['result'], true);
         if (!isset($reqData['session_key'])) {
-//            return json(ret_message('requestTokenFailed'));
-            return $res['result'];
+            return json(ret_message('requestTokenFailed'));
+//            return $res['result'];
         }
         $sessionKey = $reqData['session_key'];
 
@@ -234,7 +61,7 @@ class Index extends Base
          */
         $signature2 = sha1($rawData . $sessionKey);
 
-//        if ($signature2 !== $signature) return ret_message("signNotMatch");
+        if ($signature2 !== $signature) return ret_message("signNotMatch");
 
         /**
          *
@@ -242,15 +69,14 @@ class Index extends Base
          * 解得的信息中也包括openid, 也需要与第4步返回的openid匹配. 解密失败或不匹配应该返回客户相应错误.
          * （使用官方提供的方法即可）
          */
-        $pc = new WXBizDataCrypt($wx_request_url, $sessionKey);
+        $pc = new WXBizDataCrypt($APPID, $sessionKey);
         $errCode = $pc->decryptData($encryptedData, $iv, $data);
 
         if ($errCode !== 0) {
-//            return json(ret_message("encryptDataNotMatch"));
-            return $data;
+            return json(ret_message("encryptDataNotMatch"));
+//            print_r($errCode);
         }
-
-
+//	return json(ret_message("here"));
         /**
          * 7.生成第三方3rd_session，用于第三方服务器和小程序之间做登录态校验。为了保证安全性，3rd_session应该满足：
          * a.长度足够长。建议有2^128种组合，即长度为16B
@@ -259,122 +85,15 @@ class Index extends Base
          *
          * 以 $session3rd 为key，sessionKey+openId为value，写入memcached
          */
-        $data = json_decode($data, true);
-        $session3rd = randomFromDev(16);
+//        $data = json_decode($data, true);
+//        $session3rd = randomFromDev(16);
 
-        $data['session3rd'] = $session3rd;
-        cache($session3rd, $data['openId'] . $sessionKey);
+//        $data['session3rd'] = $session3rd;
+//        cache($session3rd, $data['openId'] . $sessionKey);
 
-//        return json($data);
-        return ['result'=>11,'message'=>'ok'];
+        return json($data);
+//        return ['result'=>11,'message'=>'ok'];
     }
 
 
-    function ret_message($message = "") {
-        if ($message == "") return ['result'=>0, 'message'=>''];
-        $ret = lang($message);
-
-        if (count($ret) != 2) {
-
-            return ['result'=>-1,'message'=>'未知错误'];
-        }
-        return array(
-            'result'  => $ret[0],
-            'message' => $ret[1]
-        );
-    }
-
-    /**
-     * 发起http请求
-     * @param string $url 访问路径
-     * @param array $params 参数，该数组多于1个，表示为POST
-     * @param int $expire 请求超时时间
-     * @param array $extend 请求伪造包头参数
-     * @param string $hostIp HOST的地址
-     * @return array    返回的为一个请求状态，一个内容
-     */
-    function makeRequest($url, $params = array(), $expire = 0, $extend = array(), $hostIp = '')
-    {
-        if (empty($url)) {
-            return array('code' => '100');
-        }
-
-        $_curl = curl_init();
-        $_header = array(
-            'Accept-Language: zh-CN',
-            'Connection: Keep-Alive',
-            'Cache-Control: no-cache'
-        );
-        // 方便直接访问要设置host的地址
-        if (!empty($hostIp)) {
-            $urlInfo = parse_url($url);
-            if (empty($urlInfo['host'])) {
-                $urlInfo['host'] = substr(DOMAIN, 7, -1);
-                $url = "http://{$hostIp}{$url}";
-            } else {
-                $url = str_replace($urlInfo['host'], $hostIp, $url);
-            }
-            $_header[] = "Host: {$urlInfo['host']}";
-        }
-
-        // 只要第二个参数传了值之后，就是POST的
-        if (!empty($params)) {
-            curl_setopt($_curl, CURLOPT_POSTFIELDS, http_build_query($params));
-            curl_setopt($_curl, CURLOPT_POST, true);
-        }
-
-        if (substr($url, 0, 8) == 'https://') {
-            curl_setopt($_curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($_curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-        }
-        curl_setopt($_curl, CURLOPT_URL, $url);
-        curl_setopt($_curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($_curl, CURLOPT_USERAGENT, 'API PHP CURL');
-        curl_setopt($_curl, CURLOPT_HTTPHEADER, $_header);
-
-        if ($expire > 0) {
-            curl_setopt($_curl, CURLOPT_TIMEOUT, $expire); // 处理超时时间
-            curl_setopt($_curl, CURLOPT_CONNECTTIMEOUT, $expire); // 建立连接超时时间
-        }
-
-        // 额外的配置
-        if (!empty($extend)) {
-            curl_setopt_array($_curl, $extend);
-        }
-
-        $result['result'] = curl_exec($_curl);
-        $result['code'] = curl_getinfo($_curl, CURLINFO_HTTP_CODE);
-        $result['info'] = curl_getinfo($_curl);
-        if ($result['result'] === false) {
-            $result['result'] = curl_error($_curl);
-            $result['code'] = -curl_errno($_curl);
-        }
-
-        curl_close($_curl);
-        return $result;
-    }
-
-    /**
-     * 读取/dev/urandom获取随机数
-     * @param $len
-     * @return mixed|string
-     */
-    function randomFromDev($len) {
-        $fp = @fopen('/dev/urandom','rb');
-        $result = '';
-        if ($fp !== FALSE) {
-            $result .= @fread($fp, $len);
-            @fclose($fp);
-        }
-        else
-        {
-            trigger_error('Can not open /dev/urandom.');
-        }
-        // convert from binary to string
-        $result = base64_encode($result);
-        // remove none url chars
-        $result = strtr($result, '+/', '-_');
-
-        return substr($result, 0, $len);
-    }
 }
