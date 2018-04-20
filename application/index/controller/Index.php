fbc4f571926c26304b82f9cf08804a08aa88024d
lindexin <dexinlin@vip.qq.com>
2018-04-20 17:07:24 +0800

    upload

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -112,10 +112,11 @@ class Index extends Base
     public function upload(){
         // 获取表单上传文件 例如上传了001.jpg
         $file = request()->file('add_image');
+        $openId = $_POST('openId');
 
         // 移动到框架应用根目录/public/uploads/ 目录下
         if($file){
-            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads' . DS . $openId);
             if($info){
                 // 成功上传后 获取上传信息
                 // 输出 jpg
