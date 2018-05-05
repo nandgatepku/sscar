d277d934f55ce5c26dc9eeebe9bac679bcb53905
lindexin <dexinlin@vip.qq.com>
2018-05-05 20:20:33 +0800

    upload

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,9 +12,35 @@ class Index extends Base
     {
         if(isset($_GET["rawdata"])){
             $weixiao = $_GET["rawdata"];
+            $res = $this->weixiao_api($weixiao);
+            $studentid = $this->res_find_studentid($res);
+            $find['student_id'] = $studentid;
+            $is_find = Db::table('info')->where($find)->select();
 
+            session_start();
+            $_SESSION['studentid'] = $studentid;
+
+            if($is_find){
+                return $this->redirect('Wechat/show_qrcode');
+            }else{
+                $have_major = $this->res_have_major($res);
+                if($have_major){
+                    $re_db =$this->res_to_db($res);
+                    if($re_db){
+                        return $this->redirect('Wechat/show_qrcode');
+                    }else{
+                        return $this->fetch('index');
+                    }
+                }else{
+                    return $this->redirect('http://weixiao.qq.com/apps/school-auth/login?media_id=gh_c5c47de251c1&app_key=116BF40DF1AFB055&redirect_uri=https://icampus.ss.pku.edu.cn/iaaa/index.php/Home/Index/appredirect//appid/sspkuuwcychwknfwfc/detail/1.html');
+                }
+
+            }
+
+        }else{
+            return $this->fetch('index');
         }
-        return $this->fetch('index');
+
     }
 
     public function wxLogin() {
@@ -72,14 +98,7 @@ class Index extends Base
         Db::table('login')->insert($insert);
 
 //	return json(ret_message("here"));
-        /**
-         * 7.生成第三方3rd_session，用于第三方服务器和小程序之间做登录态校验。为了保证安全性，3rd_session应该满足：
-         * a.长度足够长。建议有2^128种组合，即长度为16B
-         * b.避免使用srand（当前时间）然后rand()的方法，而是采用操作系统提供的真正随机数机制，比如Linux下面读取/dev/urandom设备
-         * c.设置一定有效时间，对于过期的3rd_session视为不合法
-         *
-         * 以 $session3rd 为key，sessionKey+openId为value，写入memcached
-         */
+
 //        $data = json_decode($data, true);
 //        $session3rd = randomFromDev(16);
 
@@ -192,9 +211,9 @@ class Index extends Base
                     "res"  => $result,
                     "imgadd" => $infoadd,
                 );
-//                unlink('./uploads/'.$infoadd);
-//                $p = date("Ymd");
-//                rmdir('./uploads/'.$p);
+                unlink('./uploads/'.$infoadd);
+                $p = date("Ymd");
+                rmdir('./uploads/'.$p);
 
                 return json($res_imgadd);
 
@@ -285,62 +304,131 @@ class Index extends Base
 
     }
 
-    public function signwechat(){
-        $appid = "1254398576";
-        $bucket = "sscar";
-        $secret_id = "";
-        $secret_key = "";
-        $expired = time() + 7257600;
-        $onceExpired = 0;
-        $current = time();
-        $rdm = rand();
-        $userid = "0";
-        $fileid = "tencentyunSignTest";
-
-        $srcStr = 'a='.$appid.'&b='.$bucket.'&k='.$secret_id.'&e='.$expired.'&t='.$current.'&r='.$rdm.'&u='
-            .$userid.'&f=';
-
-        $signStr = base64_encode(hash_hmac('SHA1', $srcStr, $secret_key, true).$srcStr);
-        echo $signStr."\n";
+    function unicode_decode($name){
+        $json = '{"str":"'.$name.'"}';
+        $arr = json_decode($json,true);
+        if(empty($arr)) return '';
+        return $arr['str'];
     }
 
-    public function weixiao(){
-        $apiurl = 'https://icampus.ss.pku.edu.cn/iaaa/index.php/Home/OpenApi/decode';
+    public function weixiao_api($content){
+        $url = 'https://icampus.ss.pku.edu.cn/iaaa/index.php/Home/OpenApi/decode';
 
-//        $dataurl = 'https://sscar.ptczn.cn/uploads/'.$infoadd;
-        $opt = ["appid"=>'sspkuuwcychwknfwfc','appsecret'=>'a19ff391407096a9ee3f79963a4c5bc0','content'=>'b10548eb856a95a7b1ac5719e3080bd5c2fa070219d07b371c5517e953d82b3a2b05bcc180ef6ee0e387829611ff0ad64520356e5dba64db11b7cc3e7ca5a099f1fc395450fe2bb44b48ad61e6dc1ab29e05681d60ab499908ac38957ede917f746d068d5477d17808e5554809a83bf938817627db542a0f06afd32279e9e516b7419aa2933b2ddc715bd57c94ba19d5f36c5650acb6c517c8a891f8039d27af5605b6ca8bc6b65e8a2d7fbcffc06ec60cbeb41163c182d4b2f46249da18556bf4a08fdc8f922688e4e7f497b83cf77a90aa2a3274ff337ea6e7aa8ac0da2099de4f8a5c880b35e60ec1dc60d2fcef702be3c62213bddef84ecc7f021d58fba938245f61c4eb7128a8fe68552eb1289ff211f41d640e3ccc3cd9eed5582b11c95795d4090f1cd2c3fcf7ce1019d223791797d57b15c3e9c9d4489ad5de933230b32602e4d038e76e96798b6bbfea4bb8dca478b96b60fb73e13ee7d10626d53aff28a7360460e4eb0bf1f19b0a17331b78b98d72e09c1a3f1a0f49ca615792b7a7b8d85ad0f636a978375173d6b1bdba439d8ec4e42ecb3f2cfbc9109c8bcb96e5e3a819d8925c1fe22f72655e8e37054b3715fc492c46c356096c58f95af72df8b8a4113993b4d45c2c51aedbdcff6617a8cad27017eecc5e2aca1040bf99cdbb73f2d7b78c18259abaca0e09cd1086cf5266b2f387fd4030bd3e6b5bed9fa65709fd3040862099ba63e26620e66b824ca0c1701b38d2ea242c70b251efa8e42d946436b69a272bfb4900b363a6eb31e433493d70d51819edfac6775fbae7b538075ab35559cea5dfe920a48f28e7cefb400d4803de813a86ad4ec34185661d0134f9298de6eb6f6f79d877fcd62341cf0d2e41f87de0af96d86584bddf4a16d2234c2205a805b9a004bd33dd5d54aadc9f3994d1364544051b0683e3fe79afc4155d85014da409d1c8ea6a0fbe2fcf8235325e019fbbd4f30f3be640f0fe955e28ca9727556a0d6f8a68b9ec8db5658650869d4d30f7b0003a582d584e51575d427e20c7539a3d13f11b94010a8baf8771cd24639d9ded429a1d0a5287e3e5cb54bdd50c4a430bf8f94c38416fa312'];
-        $opt_data = json_encode($opt);
+        $data = ["appid"=>'sspkuuwcychwknfwfc','appsecret'=>'a19ff391407096a9ee3f79963a4c5bc0','content'=>$content];
+//               $data = json_encode($data_tmp);
+    $opts = array(
+        CURLOPT_TIMEOUT        => 30,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+    );
 
-//        $header = array(
-//            'Host:icampus.ss.pku.edu.cn',
-//            'Content-Length:'.strlen($opt_data),
-//            'Content-Type:application/json',
-////            'Authorization:'.$auth
-//        );
+    /* 根据请求类型设置特定参数 */
+    $opts[CURLOPT_URL] = $url ;
 
-        $curl = curl_init();  //初始化
-        curl_setopt($curl,CURLOPT_URL,$apiurl);  //设置url
-//                curl_setopt($curl,CURLOPT_HTTPAUTH,CURLAUTH_BASIC);  //设置http验证方法
-        curl_setopt($curl,CURLOPT_HEADER,1);  //设置头信息
-//        curl_setopt($curl,CURLOPT_HTTPHEADER,$header);  //设置头信息
-        curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);  //设置curl_exec获取的信息的返回方式
-        curl_setopt($curl,CURLOPT_POST,1);  //设置发送方式为post请求
-        curl_setopt($curl,CURLOPT_POSTFIELDS,$opt_data);  //设置post的数据
 
-        $result = curl_exec($curl);
-        if($result === false){
-            echo curl_errno($curl);
+        $opts[CURLOPT_POST] = 1;
+        $opts[CURLOPT_POSTFIELDS] = $data;
+
+        if(is_string($data)){ //发送JSON数据
+            $opts[CURLOPT_HTTPHEADER] = array(
+                'Content-Type: application/json; charset=utf-8',
+                'Content-Length: ' . strlen($data),
+            );
         }
-//                print_r($result);
-        curl_close($curl);
-//                $result['imgadd'] = $infoadd;
 
-//        $res_imgadd = array (
-//            "res"  => $result,
-//            "imgadd" => $infoadd,
-//        );
-        print_r($result);
-//        return json($result);
+    /* 初始化并执行curl请求 */
+    $ch = curl_init();
+    curl_setopt_array($ch, $opts);
+    $data  = curl_exec($ch);
+    $error = curl_error($ch);
+    curl_close($ch);
+
+    //发生错误，抛出异常
+    if($error) throw new \Exception('请求发生错误：' . $error);
+
+//    $res =$this->res_to_db($data);
+    return $data;
+
     }
+
+    function res_find_studentid($data){
+        $student_add = strpos($data,'studentid');
+        $student_add =$student_add+12;
+        $student_add_tmp =  substr($data,$student_add);
+        $student_tmp2 = strpos($student_add_tmp,'"');
+        $studentid = substr($data,$student_add,$student_tmp2);
+
+        return $studentid;
+    }
+
+    function res_have_major($data){
+        $add = strpos($data,'major_name');
+        if($add === false){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    function res_to_db($data){
+        $uid_add = strpos($data,'uid');
+        $uid_add =$uid_add+6;
+        $uid_add_tmp =  substr($data,$uid_add);
+        $uid_tmp2 = strpos($uid_add_tmp,'"');
+        $uid = substr($data,$uid_add,$uid_tmp2);
+
+        $student_add = strpos($data,'studentid');
+        $student_add =$student_add+12;
+        $student_add_tmp =  substr($data,$student_add);
+        $student_tmp2 = strpos($student_add_tmp,'"');
+        $studentid = substr($data,$student_add,$student_tmp2);
+
+        $name_add = strpos($data,'name');
+        $name_add =$name_add+7;
+        $name_add_tmp =  substr($data,$name_add);
+        $name_tmp2 = strpos($name_add_tmp,'"');
+        $name = substr($data,$name_add,$name_tmp2);
+        $name_zi = $this->unicode_decode($name);
+
+        $major_name_add = strpos($data,'major_name');
+        $major_name_add =$major_name_add+13;
+        $major_name_add_tmp =  substr($data,$major_name_add);
+        $major_name_tmp2 = strpos($major_name_add_tmp,'"');
+        $major_name = substr($data,$major_name_add,$major_name_tmp2);
+        $major_name_zi = $this->unicode_decode($major_name);
+
+        $telephone_add = strpos($data,'telephone');
+        $telephone_add =$telephone_add+12;
+        $telephone_add_tmp =  substr($data,$telephone_add);
+        $telephone_tmp2 = strpos($telephone_add_tmp,'"');
+        $telephone = substr($data,$telephone_add,$telephone_tmp2);
+
+        $grade_add = strpos($data,'grade');
+        $grade_add =$grade_add+8;
+        $grade_add_tmp =  substr($data,$grade_add);
+        $grade_tmp2 = strpos($grade_add_tmp,'"');
+        $grade = substr($data,$grade_add,$grade_tmp2);
+
+        $where['uid'] = $uid;
+        $select = Db::table('info')->where($where)->select();
+        if($select){
+            return true;
+        }else{
+            $insert['uid'] = $uid;
+            $insert['name'] = $name_zi;
+            $insert['student_id'] = $studentid;
+            $insert['major_name'] = $major_name_zi;
+            $insert['telephone'] = $telephone;
+            $insert['grade'] = $grade;
+            $to_db = Db::table('info')->insert($insert);
+            if($to_db){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
 
 }
---
+++ application/index/controller/Wechat.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PTcZn
+ * Date: 2018/5/5
+ * Time: 15:42
+ */
+
+namespace app\index\controller;
+
+
+use app\index\common\Base;
+
+class Wechat extends Base
+{
+    public function show_qrcode(){
+        session_start();
+        $studentid = $_SESSION['studentid'];
+        $qrcode = $this->get_qrcode($studentid);
+
+        $this->assign('qrcode', $qrcode);
+        return $this->fetch('show');
+
+    }
+
+    public function signwechat(){
+        $appid = "1254398576";
+        $bucket = "sscar";
+        $secret_id = "";
+        $secret_key = "";
+        $expired = time() + 7257600;
+        $onceExpired = 0;
+        $current = time();
+        $rdm = rand();
+        $userid = "0";
+        $fileid = "tencentyunSignTest";
+
+        $srcStr = 'a='.$appid.'&b='.$bucket.'&k='.$secret_id.'&e='.$expired.'&t='.$current.'&r='.$rdm.'&u='
+            .$userid.'&f=';
+
+        $signStr = base64_encode(hash_hmac('SHA1', $srcStr, $secret_key, true).$srcStr);
+        echo $signStr."\n";
+    }
+
+    public function get_token(){
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx3dd12da36570cd80&secret=7799fedb17fd1463543704571be52cb4';
+        $curl = curl_init(); // 启动一个CURL会话
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+//        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);  // 从证书中检查SSL加密算法是否存在
+        $tmpInfo = curl_exec($curl);     //返回api的json对象
+        //关闭URL请求
+        curl_close($curl);
+
+        $token_add = strpos($tmpInfo,'access_token');
+        $token_add =$token_add+15;
+        $token_add_tmp =  substr($tmpInfo,$token_add);
+        $token_tmp2 = strpos($token_add_tmp,'"');
+        $token = substr($tmpInfo,$token_add,$token_tmp2);
+
+        return $token;
+    }
+
+    public function get_qrcode($studentid=1701210403){
+        $token = $this->get_token();
+        $url = 'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token='.$token;
+
+        $data_tmp = ["scene"=>$studentid];
+        $data = json_encode($data_tmp);
+        $opts = array(
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+        );
+
+        /* 根据请求类型设置特定参数 */
+        $opts[CURLOPT_URL] = $url ;
+
+
+        $opts[CURLOPT_POST] = 1;
+        $opts[CURLOPT_POSTFIELDS] = $data;
+
+        if(is_string($data)){ //发送JSON数据
+            $opts[CURLOPT_HTTPHEADER] = array(
+                'Content-Type: application/json; charset=utf-8',
+                'Content-Length: ' . strlen($data),
+            );
+        }
+
+        /* 初始化并执行curl请求 */
+        $ch = curl_init();
+        curl_setopt_array($ch, $opts);
+        $data  = curl_exec($ch);
+        $error = curl_error($ch);
+        curl_close($ch);
+
+        //发生错误，抛出异常
+        if($error) throw new \Exception('请求发生错误：' . $error);
+
+        return $data;
+    }
+
+}
